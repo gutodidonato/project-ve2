350870e38041421cc7124a2efa96c2f8176c3b79
gutodidonato <gutodidonato@gmail.com>
2025-06-02 19:45:41 -0300

    feat: start project ve2

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from './components/navBar/Navbar';
+import Hero from './components/hero/Hero';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      
+    </>
+  );
+    
+}
+export default App;
---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,17 @@
+
+
+function Hero() {
+  return (
+    <>
+      <div class="w-screen h-screen flex items-center justify-center bg-cover bg-center relative bg-escuro-vermelho">
+        <div class="w-screen h-100 flex items-center justify-center bg-cover bg-center relative bg-escuro-vermelho overflow-hidden">
+          <div class= "card">
+          </div>
+        </div>
+      </div>
+      
+    </>
+  );
+    
+}
+export default Hero;
