d0a81d1a274f967db2a1149cc19e5dc4221c0bdc
gutodidonato <gutodidonato@gmail.com>
2025-06-02 20:58:05 -0300

    feat: ajuste gradient + animation

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -4,9 +4,10 @@ function Hero() {
   return (
     <>
       <div class="w-screen h-screen flex items-center justify-center bg-cover bg-center relative bg-escuro-vermelho">
-        <div class="w-screen h-100 flex items-center justify-center bg-cover bg-center relative bg-escuro-vermelho overflow-hidden">
-          <div class= "card">
+        <div class="w-screen h-120 flex items-center justify-center bg-cover bg-center relative bg-escuro-vermelho overflow-hidden">
+          <div class= "card absolute">
           </div>
+
         </div>
       </div>
       
