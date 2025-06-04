f51186eefa3281c0a81c5822362da0587b486cb7
gutodidonato <gutodidonato@gmail.com>
2025-06-03 22:11:43 -0300

    feat: ajuste 1

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,13 +1,15 @@
-
+import Card from "./components/Card";
 
 function Hero() {
   return (
     <>
-      <div class="w-screen h-screen flex items-center justify-center bg-cover bg-center relative bg-escuro-vermelho">
-        <div class="w-screen h-120 flex items-center justify-center bg-cover bg-center relative bg-escuro-vermelho overflow-hidden">
+      <div class="w-screen h-screen flex items-center justify-center bg-cover bg-center relative radiant-vermelho">
+        <div class="w-screen h-120 flex items-center justify-center bg-cover bg-center relative overflow-hidden">
           <div class= "card absolute">
           </div>
-
+          <div>
+            <Card></Card>
+          </div>
         </div>
       </div>
       
