70cb9ae9ad96f79c3b229cb4cd6e14a135b33743
gutodidonato <gutodidonato@gmail.com>
2025-06-10 20:24:24 -0300

    feat: terminar amanhã

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import ScrollManager from "./components/ScrollManager";
 import Menu from "./components/Menu"
 import { EffectComposer, Bloom } from '@react-three/postprocessing';
 import CardCarrossel from "./components/CardCarrossel";
-
+import { Wall } from "./components/Wall";
 import Experience from './components/Experience';
 import Navbar from './components/Navbar';
 
@@ -15,6 +15,7 @@ function App() {
   const [menuOpened, setMenuOpened] = useState(false);
   const [page, setPage] = useState(0)
 
+  let intensity = (section == 0 || section == 1)? 0 : 2;
 
   return (
     <>
@@ -24,10 +25,12 @@ function App() {
           <ScrollManager section={section} onSectionChange={setSection}/>
           <EffectComposer>
           <Experience/>
-            <Bloom intensity={2} luminanceThreshold={0.1} luminanceSmoothing={0.9}/>
+          
+            <Bloom intensity={intensity} luminanceThreshold={0.1} luminanceSmoothing={0.9}/>
           </EffectComposer>
           <Scroll html>
             <Interface/>
+            <div className="h-screen w-screen"></div>
             <div className="h-screen w-screen"></div>
             <CardCarrossel/>
           </Scroll>
@@ -37,7 +40,6 @@ function App() {
             menuOpened = {menuOpened}
             setMenuOpened = {setMenuOpened}
             />
-            <Navbar/>
     </>
   );
     
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,38 +1,41 @@
 import { useGLTF, useScroll } from '@react-three/drei';
 import { OrbitControls } from '@react-three/drei';
 import { useFrame } from '@react-three/fiber';
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, useState } from 'react';
 import gsap from 'gsap';
 import * as THREE from 'three';
+import { Wall } from "./Wall";
 
-const Model = () => {
+const Model = (props) => {
   const { scene } = useGLTF('/models/delorean_dmc12__retro_wave_art.glb');
 
-  useEffect(() => {
-    scene.traverse((child) => {
-      if (child.isMesh) {
-        child.material.emissiveIntensity = 1.5;
-        child.material.toneMapped = false;
-      }
-    });
-  }, [scene]);
+useEffect(() => {
+  scene.traverse((child) => {
+    if (child.isMesh) {
+      child.material.emissiveIntensity = 1.5;
+      child.material.toneMapped = false;
+      child.material.transparent = true;
+      child.material.opacity = props.opacity !== undefined ? props.opacity : 1;
+    }
+  });
+}, [scene, props.opacity]);
 
-  return <primitive object={scene} scale={1.0} />;
+  return <primitive object={scene} props scale={1.0} />;
 };
 export const Experience = () => {
   const meshRef = useRef();
   const lightRef = useRef();
   const scroll = useScroll();
   const fadePlaneRef = useRef();
+  const [modelOpacity, setModelOpacity] = useState(1);
+  const [wallOpacity, setWallOpacity] = useState(0);
 
-
-  const lightAnimatedRef = useRef(false);
 
   const cameraStages = [
     {
       start: 0.0,
       end: 0.125,
-      from: new THREE.Vector3(12, -0.78, -7),
+      from: new THREE.Vector3(10, -0.78, -7),
       to: new THREE.Vector3(15.66, -0.83, -6.31),
       lookFrom: new THREE.Vector3(0, 0, 0),
       lookTo: new THREE.Vector3(1, 0, 0),
@@ -41,30 +44,37 @@ export const Experience = () => {
       start: 0.125,
       end: 0.25,
       from: new THREE.Vector3(15.66, -0.83, -6.31),
-      to: new THREE.Vector3(0.62, 0.62, 15.58),
+      to: new THREE.Vector3(0.62, 3, 15.58),
       lookFrom: new THREE.Vector3(1, 0, 0),
       lookTo: new THREE.Vector3(0, 0, 0),
     },
     {
       start: 0.25,
       end: 0.375,
-      from: new THREE.Vector3(0.62, 0.62, 15.58),
-      to: new THREE.Vector3(-7.41, -0.57, 4.47),
+      from: new THREE.Vector3(0.62, 3, 15.58),
+      to: new THREE.Vector3(-7.41, -1, 10),
       lookFrom: new THREE.Vector3(0, 0, 0),
       lookTo: new THREE.Vector3(0, 1, 0),
     },
     {
       start: 0.375,
       end: 0.5,
-      from: new THREE.Vector3(-7.41, -0.57, 4.47),
+      from: new THREE.Vector3(-7.41, -1, 10),
       to: new THREE.Vector3(-30, 3, -7),
       lookFrom: new THREE.Vector3(0, 1, 0),
       lookTo: new THREE.Vector3(10, 3, 3),
     },
+
   ];
 
   useFrame((state) => {
     const y = scroll.offset;
+    console.log(`Scroll atual: ${scroll.offset}`)
+
+    const targetUseModel = y <= 0.5;
+
+    setModelOpacity(prev => THREE.MathUtils.lerp(prev, targetUseModel ? 1 : 0, 0.1));
+    setWallOpacity(prev => THREE.MathUtils.lerp(prev, targetUseModel ? 0 : 1, 0.1));
 
     for (const stage of cameraStages) {
       if (y >= stage.start && y <= stage.end) {
@@ -79,41 +89,58 @@ export const Experience = () => {
       }
     }
 
+    if (y < 0.125)
+      gsap.to(lightRef.current.position, { x: 15, y: 0, z: 0, duration: 1.2, ease: 'power2.out' });
+      gsap.to(lightRef.current, { intensity: 2, duration: 1.2 });
 
-    if (y > 0.125 && !lightAnimatedRef.current) {
-      lightAnimatedRef.current = true;
-      gsap.to(lightRef.current.position, { x: -2, y: 8, duration: 1.2, ease: 'power2.out' });
-      gsap.to(lightRef.current, { intensity: 2, duration: 1.5, ease: 'power2.out' });
+    if (y > 0.125 && y < 0.25) {
+      gsap.to(lightRef.current.position, { x: 15, y: 30, z: 3, duration: 5 });
+      gsap.to(lightRef.current, { intensity: 1.2, duration: 1.2 });
     }
-    if (y < 0.125 && lightAnimatedRef.current) {
-      lightAnimatedRef.current = false;
-      gsap.to(lightRef.current.position, { x: 3, y: 5, duration: 1.2, ease: 'power2.inOut' });
-      gsap.to(lightRef.current, { intensity: 1, duration: 1.2, ease: 'power2.inOut' });
+    if (y > 0.25) {
+      gsap.to(lightRef.current.position,{ x: -5, y: 8, z: 3, duration: 5 });
+      gsap.to(lightRef.current, { intensity: 2, duration: 1.2 });
     }
 
+   if (y > 0.6) {
 
+      state.camera.position.set(0, 0.5, 0);
+      state.camera.lookAt(new THREE.Vector3(0, 0, 0));
+    }
+
+    
+    
     if (fadePlaneRef.current) {
       const camera = state.camera;
       fadePlaneRef.current.position.copy(camera.position).add(camera.getWorldDirection(new THREE.Vector3()).multiplyScalar(1));
       fadePlaneRef.current.quaternion.copy(camera.quaternion);
-      fadePlaneRef.current.material.opacity = THREE.MathUtils.clamp((y - 0.35) / 0.2, 0, 1);
+      if (y < 0.5){
+        fadePlaneRef.current.material.opacity = THREE.MathUtils.clamp((y - 0.35) / 0.1, 0, 1);
+      } if (y >= 0.6) {
+        fadePlaneRef.current.material.opacity = THREE.MathUtils.clamp(1 - (y - 0.5) / 0.2, 0, 1);
+      }
     }
+  
+      
 
   })
 
-  return (
-    <>
+
+    return (
+      <>
       <ambientLight intensity={1} />
-      <directionalLight ref={lightRef} position={[3, 5, 1]} intensity={1} castShadow />
+      <directionalLight ref={lightRef} position={[15, 1, 0]} intensity={2} castShadow />
       <mesh ref={fadePlaneRef} position={[0, 0, 0]}>
         <planeGeometry args={[100, 100]} />
         <meshBasicMaterial color="black" transparent opacity={0} />
       </mesh>
       <group ref={meshRef} position={[0, -1.5, 0]}>
-        <Model />
+        <Model opacity={modelOpacity} />
+      </group>
+      <group>
+        <Wall opacity={wallOpacity} />
       </group>
     </>
   );
-};
-
+  }
 export default Experience;
